5a41a9fcc4e67b3d6a28200dec785aa7f474b862
anatskk <anacaroline25190@gmail.com>
2024-08-26 21:34:48 -0300

    pagina adocao

--- ePet/obj/Debug/net8.0/ePet.AssemblyInfo.cs
+++ ePet/obj/Debug/net8.0/ePet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ePet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e8297a2423934f0a2d8d1e716c21b7395992b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("ePet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ePet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

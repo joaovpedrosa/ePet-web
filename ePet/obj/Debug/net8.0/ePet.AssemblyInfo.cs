b3ae78538829b0f3b617cc744f7b25e588426627
anatskk <anacaroline25190@gmail.com>
2024-09-02 20:14:55 -0300

    Erros corrigidos

--- ePet/obj/Debug/net8.0/ePet.AssemblyInfo.cs
+++ ePet/obj/Debug/net8.0/ePet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ePet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e402015723d346fc03262281cbcd362609214ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b1c62feabc4b169454e57ec23181176bc6f14f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ePet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ePet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

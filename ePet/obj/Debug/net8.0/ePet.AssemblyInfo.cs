dd0bbc384f56a3954035357d6b6e3f215600df49
Anatskk <anacaroline25190@gmail.com>
2024-09-30 19:46:14 -0300

    tabelas adm

--- ePet/obj/Debug/net8.0/ePet.AssemblyInfo.cs
+++ ePet/obj/Debug/net8.0/ePet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ePet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45de02cd08a648750fac464ff6ba3b6da10c934")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0676e0d3c1e886c353885978115b122c421e1a2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ePet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ePet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

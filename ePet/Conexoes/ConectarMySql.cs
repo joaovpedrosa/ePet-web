e915b3ce0290f38263fce624ce4bbe4420b6c2c6
Monolokaio <kaioeduardo401@outlook.com>
2024-10-29 22:42:03 -0300

    feat- Pasta Recriada, com nome correto.

---
+++ ePet/Conexoes/ConectarMySql.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ePet.Models;
+using MySql.Data.MySqlClient;
+
+namespace ePet.Conexões
+{
+    public class ConectarMySql
+    {
+        public static MySqlConnection getConexao()
+        {
+            return new MySqlConnection(
+                Configuration().GetConnectionString("Default"));
+        }
+
+        private static IConfigurationRoot Configuration()
+        {
+            IConfigurationBuilder builder =
+                new ConfigurationBuilder().SetBasePath(
+                    Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            return builder.Build();
+        }
+    }
+}
+
